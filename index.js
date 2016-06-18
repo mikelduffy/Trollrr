b857dd20eb675ed910351d5411e2220a157eb11f
Mikel Duffy <mduffy23@gmail.com>
2016-06-17 20:56:36 -0700

    asdf

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ var express = require('express');
 var app = express();
 var Snoocore = require('snoocore');
 // var trollResponse = require('./controllers/snoocore.js');
+var bodyParser = require('body-parser')
 
-app.get('/', function(req, res) {
-  res.send('you can not troll this troll');
-});
 
-app.post('/mention/', function(req, res) {
-  // trollResponse();
-  // var resp = {'text': req.body}
-  console.log(req);
-});
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+  console.log(req.body) // populated!
+  next()
+})
+
+
+
+// app.get('/', function(req, res) {
+//   res.send('you can not troll this troll');
+// });
+
+// app.post('/mention/', function(req, res) {
+//   // trollResponse();
+//   // var resp = {'text': req.body}
+//   console.log(req);
+// });
 
 // app.get('/mention/', function(req, res) {
 //   // trollResponse();
