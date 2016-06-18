8986c83ac03ccce84b985f5217c1384e831a8cf5
Mikel Duffy <mduffy23@gmail.com>
2016-06-17 20:19:07 -0700

    update snoocore

--- controllers/snoocore.js
+++ controllers/snoocore.js
@@ -3,13 +3,13 @@ var Snoocore = require('snoocore');
 // Our new instance associated with a single account.
 // It takes in various configuration options.
 var reddit = new Snoocore({
-  userAgent: '/u/trollrr troll all the trolls, // unique string identifying the app
+  userAgent: '/u/trollrr troll all the trolls', // unique string identifying the app
   oauth: {
     type: 'script',
-    key: '', // OAuth client key (provided at reddit app)
-    secret: '', // OAuth secret (provided at reddit app)
-    username: '', // Reddit username used to make the reddit app
-    password: '', // Reddit password for the username
+    key: $REDDIT_KEY, // OAuth client key (provided at reddit app)
+    secret: $REDDIT_SECRET, // OAuth secret (provided at reddit app)
+    username: 'trollrr', // Reddit username used to make the reddit app
+    password: $REDDIT_PASSWORD, // Reddit password for the username
     // The OAuth scopes that we need to make the calls that we
     // want. The reddit documentation will specify which scope
     // is needed for evey call
@@ -20,3 +20,5 @@ var reddit = new Snoocore({
 reddit('/api/v1/me').get().then(function(result) {
   console.log(result);
 });
+
+module.exports = reddit;
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var reddit = require('controllers/snoocore');
+
+app.get('/', function(req, res) {
+  res.send('you can not troll this troll');
+});
 
 app.get('/mention/', function(req, res) {
+  reddit();
   res.send('you posted these params: ' + req.query.id);
+
 });
 
 app.listen(8080, function () {
