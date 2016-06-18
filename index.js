2e1d3980fce5d89a4b148f293973c3888114abd5
Mikel Duffy <mduffy23@gmail.com>
2016-06-17 20:06:19 -0700

    update server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('test response!');
+app.get('*', function(req, res) {
+  res.send('you posted these params: ' + req.query.id);
 });
 
 app.listen(8080, function () {
-  console.log("Trollrr is running on port 80!");
+  console.log("Trollrr is running on port 8080!");
 });
