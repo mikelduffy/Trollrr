f98761333cfb791d68a396c93f3ace139dcacc38
Mikel Duffy <mduffy23@gmail.com>
2016-06-22 12:29:32 -0700

    Implements lambda function.

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ Created by Vipul Patil, Andy Khong, and Mikel Duffy for the 2016 BallmerPeakatho
 ## How it Works
 * AWS Lambda function watches inbox for new named mentions every minute.
 * When new mention arrives, it grabs the parent comment and submits it to an online chatbot, scrapes the response, then submits it in a comment responding to the troll.
+
+## Todo
+* Expand scope of taking action on the first unread message, to all unread messages.
+* Fix logic to respond to parent comment.
+* Minimize reddit API calls.
+* Store history in dynamo DB, and check to avoid double commenting.
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 var Snoocore = require('snoocore');
 var credentials = require('./controllers/credentials');
 var scraper = require('./controllers/scraper');
@@ -5,49 +6,51 @@ var reddit;
 var messageId;
 var parentId;
 
+exports.myHandler = function(event, context, callback) {
 // Get all the secrets.
-Promise.all([
-  credentials.getKey(),
-  credentials.getSecret(),
-  credentials.getPassword()
-]).then(([key, secret, password]) => {
-  credentials.key = key;
-  credentials.secret = secret;
-  credentials.password = password;
-}).then(() => {
-// Start Reddit Oauth Session
-  reddit = new Snoocore({
-    userAgent: '/u/trollrr troll all the trolls',
-    oauth: {
-      type: 'script',
-      key: credentials.key,
-      secret: credentials.secret,
-      username: 'trollrr',
-      password: credentials.password,
-      scope: [ 'read', 'vote', 'privatemessages', 'submit' ]
-    }
-  })
-// Get unread messages, and process the first.
-  return reddit('/message/unread').listing()
-}).then((listing) => {
-// Only respond to comments, not the root thread.
-    if (/^(t1_)/.test(listing.children[0].data.parent_id)) {
-      messageId = listing.children[0].data.id;
-      parentId = listing.children[0].data.parent_id;
-      var trollCommentContext = listing.children[0].data.context.replace(/([\/A-Za-z0-9_]*)((.|\n)*)/g, "$1");
-      return reddit(trollCommentContext).get({context: 1})
-    }
-}).then((listing) => {
-// Get parent comment.
-  var trollComment = listing[1].data.children[0].data.body.replace(/^\s+|\s+$/g, '');
-// Get response comment.
-  return scraper.returnMessage(trollComment);
-}).then((comment) => {
-// Post comment.
-  return reddit('/api/comment').post({thing_id: parentId, text: comment});
-})
-.then(() => {
-// Mark message as read.
-  return reddit('/api/read_message').post({id: messageId})
-})
-.catch((err) => {console.log(err);})
+  Promise.all([
+    credentials.getKey(),
+    credentials.getSecret(),
+    credentials.getPassword()
+  ]).then(([key, secret, password]) => {
+    credentials.key = key;
+    credentials.secret = secret;
+    credentials.password = password;
+  }).then(() => {
+  // Start Reddit Oauth Session
+    reddit = new Snoocore({
+      userAgent: '/u/trollrr troll all the trolls',
+      oauth: {
+        type: 'script',
+        key: credentials.key,
+        secret: credentials.secret,
+        username: 'trollrr',
+        password: credentials.password,
+        scope: [ 'read', 'vote', 'privatemessages', 'submit' ]
+      }
+    })
+  // Get unread messages, and process the first.
+    return reddit('/message/unread').listing()
+  }).then((listing) => {
+  // Only respond to comments, not the root thread.
+      if (/^(t1_)/.test(listing.children[0].data.parent_id)) {
+        messageId = listing.children[0].data.id;
+        parentId = listing.children[0].data.parent_id;
+        var trollCommentContext = listing.children[0].data.context.replace(/([\/A-Za-z0-9_]*)((.|\n)*)/g, "$1");
+        return reddit(trollCommentContext).get({context: 1})
+      }
+  }).then((listing) => {
+  // Get parent comment.
+    var trollComment = listing[1].data.children[0].data.body.replace(/^\s+|\s+$/g, '');
+  // Get response comment.
+    return scraper.returnMessage(trollComment);
+  }).then((comment) => {
+  // Post comment.
+    return reddit('/api/comment').post({thing_id: parentId, text: comment});
+  // }).then(() => {
+  // // Mark message as read.
+  //   return reddit('/api/read_message').post({id: messageId})
+  }).then(() => {
+    console.log('Success!');
+  }).catch((err) => {console.log(err);})
+}
